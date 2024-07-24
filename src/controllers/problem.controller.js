7764efd62db13ddbc9167a37cfbc1ce05ff5b3f8
Shreerajsingh <shreerajsinghsisodiya@gmail.com>
2024-07-25 03:32:46 +0530

    Feat: added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,23 +3,33 @@ function pingProblemController(req, res) {
 }
 
 function addProblem(req, res) {
-
+    return res.status(501).json({
+        message: 'Node Implemented'
+    });
 }
 
 function getProblem(req, res) {
-    
+    return res.status(501).json({
+        message: 'Node Implemented'
+    });
 }
 
 function getProblems(req, res) {
-    
+    return res.status(501).json({
+        message: 'Node Implemented'
+    });
 }
 
 function updateProblem(req, res) {
-    
+    return res.status(501).json({
+        message: 'Node Implemented'
+    });
 }
 
 function deleteProblem(req, res) {
-    
+    return res.status(501).json({
+        message: 'Node Implemented'
+    });
 }
 
 
